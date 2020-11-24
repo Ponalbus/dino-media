d1c5089f35a068a4923f56384d6d7e36cb5c355a
Ponalbus <59609727+Ponalbus@users.noreply.github.com>
2020-11-24 19:38:33 +0300

    Add files via upload

---
+++ main-mobile.js
@@ -0,0 +1,43 @@
+const mediaQuery = window.matchMedia('(min-width: 320px)')
+ 
+// Check if the media query is true
+if (mediaQuery.matches) {
+  // Then trigger an alert
+  $('.sale-box-common').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+}
+
+		
